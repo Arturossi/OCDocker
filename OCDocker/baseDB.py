c4aa31a3db1ffa30df2889450c123e70a0056d35
Doll <arturossi10@gmail.com>
2022-08-15 23:08:46 -0300

    DUDEz database procesing fix

--- OCDocker/baseDB.py
+++ OCDocker/baseDB.py
@@ -416,7 +416,7 @@ def __core_prepare(dir, overwrite, archive, sanitize, spacing):
     # For each dir to be processed
     for dir in processDirs:
         # If overwrite mode is on or there is not the same amount of box files as folders in vinaFiles folder
-        if boxCount == 0 or len(glob(f"{dir}/vinaFiles/*")) == boxCount or overwrite:
+        if boxCount == 0 or len(glob(f"{dir}/vinaFiles/*")) != boxCount or overwrite:
             # Check if the archive is dudez
             if archive == "dudez":
                 # Create the vina inputs from the boxes
@@ -427,7 +427,7 @@ def __core_prepare(dir, overwrite, archive, sanitize, spacing):
         else:
             octools.print_info(f"The protein '{dir}' already has its vina file generated, skipping its execution.")
         # If overwrite mode is on or there is not the same amount of box files as folders in vinaFiles folder
-        if boxCount == 0 or len(glob(f"{dir}/plantsFiles/*")) == boxCount or overwrite:
+        if boxCount == 0 or len(glob(f"{dir}/plantsFiles/*")) != boxCount or overwrite:
             # Check if fligand is a list type
             if isinstance(fligand, list):
                 # Since it is a list, we need to generate the plants files for each ligand
