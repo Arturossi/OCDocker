dcf56dcd275e78111e5d28b9ff85ed9b42f2a5fd
Doll <arturossi10@gmail.com>
2022-11-10 20:44:05 -0300

    Optimizations

--- OCDocker/baseDB.py
+++ OCDocker/baseDB.py
@@ -577,27 +577,18 @@ def __core_prepare(path: str, overwrite: bool, archive: str, sanitize: bool, spa
         
         # If overwrite mode is on or there is not the same amount of box files as folders in vinaFiles folder
         if boxCount == 0 or len(glob(f"{processDir}/vinaFiles/*")) != boxCount  or len(glob(f"{processDir}/vinaFiles/*")) == 0 or overwrite:
-            # Check if the archive is dudez
-            if archive == "dudez":
-                # Create the vina inputs from the boxes
-                ocvina.generate_vina_files_database(processDir, fin, boxPath = f"{processDir}/boxes/box0.pdb")
-            else:
-                # Create the vina inputs from the boxes
-                ocvina.generate_vina_files_database(processDir, fin)
+            # Create the vina inputs from the boxes
+            print(f"ocvina.generate_vina_files_database('{processDir}', '{fin}', boxPath = '{processDir}/boxes/box0.pdb')")
+            ocvina.generate_vina_files_database(processDir, fin, boxPath = f"{processDir}/boxes/box0.pdb")
         else:
             octools.print_info(f"The protein '{processDir}' already has its vina file generated, skipping its execution.")
 
         # If overwrite mode is on or there is not the same amount of box files as folders in vinaFiles folder
         if boxCount == 0 or len(glob(f"{processDir}/plantsFiles/*")) != boxCount or len(glob(f"{processDir}/plantsFiles/*")) == 0 or overwrite:
-            # Check if the archive is dudez
-            if archive == "dudez":
-                # Extract the molecule name from dir
-                ligandName = os.path.splitext(os.path.basename(processDir))[0]
-                # Set the fligand variable to the dir + ligandName + .mol2
-                fligand = f"{processDir}/{ligandName}.mol2"
-            else:
-                fligand = ""
+            # Set the fligand variable to the dir + ligandName + .mol2
+            fligand = f"{processDir}/ligand.mol2"
             # Create the PLANTS inputs from the boxes
+            print(f"ocplants.generate_plants_files_database('{processDir}', '{fin}', boxPath = '{processDir}/boxes/box0.pdb')")
             ocplants.generate_plants_files_database(processDir, preparedReceptor, fligand, spacing, boxPath = f"{processDir}/boxes/box0.pdb")
         else:
             octools.print_info(f"The protein '{processDir}' already has its PLANTS file generated, skipping its execution.")
