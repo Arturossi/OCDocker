1b600034702d2a72f50070f921ed29d5e129082b
Doll <arturossi10@gmail.com>
2022-11-26 20:07:17 -0300

    PLANTS docking function debug

--- OCDocker/baseDB.py
+++ OCDocker/baseDB.py
@@ -645,7 +645,7 @@ def __core_prepare(path: str, overwrite: bool, archive: str, sanitize: bool, spa
             # Start the lock with statement
             with lock:
                 # Create the smina inputs
-                ocsmina.gen_smina_conf(f"{processDir}/sminaFiles/conf_smina.conf", preparedReceptorPdbqt)
+                ocsmina.gen_smina_conf(f"{processDir}/boxes", f"{processDir}/sminaFiles/conf_smina.conf", preparedReceptorPdbqt)
         else:
             octools.print_info(f"The protein '{processDir}' already has its smina file generated, skipping its execution.")
 
@@ -2597,13 +2597,16 @@ def run_dock(archive: str, dockingAlgorithm: str, overwrite: bool = False) -> in
     
     # For each dir in dirs, let's grab all ligands
     for ptnDir in ptnDirs:
+        print(ptnDir)
         # Parameterize paths
         ligands = f"{ptnDir}/compounds/ligands"
-        decoys = f"{ptnDir}/compounds/decoys"
-        candidates = f"{ptnDir}/compounds/candidates"
+        #decoys = f"{ptnDir}/compounds/decoys"
+        #candidates = f"{ptnDir}/compounds/candidates"
 
         # Append to the complex list the merged ligandAlternative list with the list with ligands, decoys and candidates. This is made because each receptor must have its own list of ligands, decoys and candidates, otherwise the docking could be done with the same ligands, decoys and candidates for all receptors making everything out of control.
-        complexList.append((ptnDir, glob(f"{ligands}/*") + glob(f"{decoys}/*") + glob(f"{candidates}/*")))
+        complexList.append((ptnDir, glob(f"{ligands}/*")))# + glob(f"{decoys}/*") + glob(f"{candidates}/*")))
+        break
+        
         
     # Decide if multprocessing will be used
     if args.multiprocess:
