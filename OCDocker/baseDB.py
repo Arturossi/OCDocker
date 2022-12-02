4a9ff088b4a75c75ae25d0862b078e347d67c9da
Doll <arturossi10@gmail.com>
2022-12-02 00:11:50 -0300

    Bugfix for reading logs within new vaex routine

--- OCDocker/baseDB.py
+++ OCDocker/baseDB.py
@@ -2489,7 +2489,6 @@ def __core_merge_descriptors_in_dataframe(processDirPackage: Tuple[str, str]) ->
     # Check if there is the receptor json, if yes, load it
     if os.path.isfile(receptor_descriptor_path):
         receptor_descriptors = ocr.read_descriptors_from_json(receptor_descriptor_path, returnVaex = True)
-        print("data receptor descr: " + receptor_descriptors)
     else:
         receptor_descriptors = None
         _ = errors.file_do_not_exist(f"The file '{receptor_descriptor_path}' does not exist!")
@@ -2497,7 +2496,6 @@ def __core_merge_descriptors_in_dataframe(processDirPackage: Tuple[str, str]) ->
     # Check if there is the ligand json, if yes, load it
     if os.path.isfile(ligand_descriptor_path):
         ligand_descriptors = ocl.read_descriptors_from_json(ligand_descriptor_path, returnVaex = True)
-        print("data ligand desc: " + ligand_descriptors)
     else:
         ligand_descriptors = None
         _ = errors.file_do_not_exist(f"The file '{ligand_descriptor_path}' does not exist!")
