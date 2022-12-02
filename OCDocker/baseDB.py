2568ea42657a3061e5165b7b5ab632cda2384259
Doll <arturossi10@gmail.com>
2022-12-02 00:55:50 -0300

    Bugfix for reading logs within new vaex routine

--- OCDocker/baseDB.py
+++ OCDocker/baseDB.py
@@ -2506,7 +2506,7 @@ def __core_merge_descriptors_in_dataframe(processDirPackage: Tuple[str, str]) ->
     # If the receptor descriptor is not empty
     if receptor_descriptors: # type: ignore
         # Rename the 'Name' column to 'Protein'
-        ligand_descriptors.rename(columns = { "Name": "Protein" }) # type: ignore
+        #ligand_descriptors.rename("Name": "Protein") # type: ignore
         # Merge the receptor descriptors
         df = df.join(receptor_descriptors) # type: ignore
         # Drop the 'Path' column
@@ -2515,7 +2515,7 @@ def __core_merge_descriptors_in_dataframe(processDirPackage: Tuple[str, str]) ->
     # If the ligand descriptor is not empty
     if ligand_descriptors: # type: ignore
         # Rename the 'Name' column to 'Ligand'
-        ligand_descriptors.rename(columns = { "Name": "Ligand" }) # type: ignore
+        #ligand_descriptors.rename("Name": "Ligand") # type: ignore
         # Drop the 'Path' column
         ligand_descriptors.drop("Path", inplace = True) # type: ignore
         # Merge the ligand descriptors
