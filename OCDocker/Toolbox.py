a167e7a8802b5f8eb77187f3c649b03cc17b81fc
Doll <arturossi10@gmail.com>
2022-12-03 14:35:48 -0300

    Improvement for file reading performace using mmap

--- OCDocker/Toolbox.py
+++ OCDocker/Toolbox.py
@@ -948,31 +948,7 @@ def read_mmap(file_name: str) -> Generator[str, None, None]:
     # Open file for reading in binary mode
     with open(file_name, 'rb') as read_obj:
         with mmap.mmap(read_obj.fileno(), 0, access=mmap.ACCESS_READ) as mmap_obj:
-            # Get the current position of pointer i.e eof
-            pointer_location = mmap_obj.tell()
-            # Create a buffer to keep the last read line
-            buffer = bytearray()
-            # Loop till pointer reaches the top of the file
-            while pointer_location <= os.SEEK_END:
-                # Move the file pointer to the location pointed by pointer_location
-                mmap_obj.seek(pointer_location)
-                # Shift pointer location by +1
-                pointer_location = pointer_location + 1
-                # read that byte / character
-                new_byte = mmap_obj.read(1)
-                # If the read byte is new line character then it means one line is read
-                if new_byte == b'\n':
-                    # Fetch the line from buffer and yield it
-                    yield buffer.decode()[::-1]
-                    # Reinitialize the byte array to save next line
-                    buffer = bytearray()
-                else:
-                    # If last read character is not eol then add it in buffer
-                    buffer.extend(new_byte)
-            # As file is read completely, if there is still data in buffer, then its the first line.
-            if len(buffer) > 0:
-                # Yield the first line too
-                yield buffer.decode()[::-1]
+            yield mmap_obj.readline().decode('utf-8')
 
 def read_reverse_order_mmap(file_name: str) -> Generator[str, None, None]:
     '''Read a file in reverse order using mmap.
