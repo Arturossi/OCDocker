de1111eb7844c3b01f9461e473f871bc7e7103c1
Doll <arturossi10@gmail.com>
2023-01-23 11:25:12 -0300

    Fix for parallel merging

--- OCDocker/Processing/Postprocessing/MergeLogs.py
+++ OCDocker/Processing/Postprocessing/MergeLogs.py
@@ -223,35 +223,35 @@ def __merge_descriptors_in_dataframe_parallel(paths: List[Tuple[str, str]], rece
 
     # Check if arguments has any elements
     if len(arguments) > 0:
-    try:
-        # Create a Thread pool with the maximum available_cores
-        with Pool(args.available_cores) as p:
-            # Perform the multi process
-            for innerData in tqdm(p.imap_unordered(__thread_merge_descriptors_in_dataframe_parallel, arguments), total = len(arguments), desc = desc):
-                # Update the dict with the result from the called function
-                ptnList.append(innerData)# Increment the counter
-                i += 1
-                # Check if the counter is greater or equal of saveChunk
-                if i >= saveChunk:
+        try:
+            # Create a Thread pool with the maximum available_cores
+            with Pool(args.available_cores) as p:
+                # Perform the multi process
+                for innerData in tqdm(p.imap_unordered(__thread_merge_descriptors_in_dataframe_parallel, arguments), total = len(arguments), desc = desc):
+                    # Update the dict with the result from the called function
+                    ptnList.append(innerData)# Increment the counter
+                    i += 1
+                    # Check if the counter is greater or equal of saveChunk
+                    if i >= saveChunk:
+                        # Convert the ptnList to a dataframe
+                        df = vaex.concat(ptnList)
+
+                        # Save it
+                        df.export_hdf5(receptorDataFile)
+
+                        # Reset the counter
+                        i = 0
+
+                    # Clear the memory
+                    gc.collect()
+
+                # If on finish and i is not 0
+                if i != 0:
                     # Convert the ptnList to a dataframe
-                    df = vaex.concat(ptnList)
+                    innerdf = vaex.concat(ptnList)
 
-                    # Save it
-                    df.export_hdf5(receptorDataFile)
-
-                    # Reset the counter
-                    i = 0
-
-                # Clear the memory
-                gc.collect()
-
-            # If on finish and i is not 0
-            if i != 0:
-                # Convert the ptnList to a dataframe
-                innerdf = vaex.concat(ptnList)
-
-                # Save the data
-                innerdf.export_hdf5(receptorDataFile)
+                    # Save the data
+                    innerdf.export_hdf5(receptorDataFile)
         except IOError as e:
             errMsg = f"Problem while merging descriptors in parallel. Exception: {e}"
             ocprint.print_error_log(errMsg, f"{logdir}/read_log_ERROR_report.log")
