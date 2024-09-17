ef607ef18b96038b54ef6872e1b748e1448fa982
arturossi <arturossi10@gmail.com>
2024-09-17 19:44:20 -0300

    Refactor apply_pca function to use df.update and optimize column addition
    This commit refactors the apply_pca function in the Data.py file. Instead of looping through columns to update the dataframe, the function now uses the df.update method for better performance. Additionally, the code has been optimized to add columns from the combined DataFrame to the original DataFrame using a for loop. This change improves the efficiency of the function.
    
    Fix potential issue with loading data in load_data function
    
    This commit fixes a potential issue in the load_data function in the Data.py file. The columns "experimental" has been added to the list of columns to be dropped when splitting the PDBbind data into training and testing sets. This change ensures that the "experimental" column is ignored during the split process.

--- OCDocker/OCScore/Utils/Data.py
+++ OCDocker/OCScore/Utils/Data.py
@@ -149,9 +149,11 @@ def apply_pca(df: pd.DataFrame, pca_model_path: str, columns_to_skip_pca: list[s
     if inplace:
         # Modify the original DataFrame in place
         df.drop(df.columns, axis = 1, inplace = True)
-        # Update the dataframe
-        df.update(combined_df)
-        # Return None
+
+        # For each column in the combined DataFrame
+        for col in combined_df.columns:
+            # Add the columns from the combined DataFrame to the original DataFrame
+            df[col] = combined_df[col].values
         return None
     else:
         # Return a new DataFrame with PCA applied
@@ -364,7 +366,7 @@ def load_data(base_models_folder: str, storage_id: int, df_path: str, optimizati
         # Split the PDBbind data into training and testing sets
         X_train, X_test, y_train, y_test = split_dataset(
             pdbbind_data.drop(
-                columns = ["receptor", "ligand", "name", "type", "db"],
+                columns = ["receptor", "ligand", "name", "type", "db", "experimental"],
                 errors = "ignore"
             ), 
             pdbbind_data["experimental"], 
